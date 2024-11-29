7b429a21b6e2d2f3183c061dab5dc9f7e9888274
Pat-TX <williams.patrick.d@gmail.com>
2024-11-29 16:33:04 -0600

    login page -- need auth

--- app/page.js
+++ app/page.js
@@ -5,7 +5,21 @@ export default function Home() {
   return (
     <div className={styles.page}>
       <main className={styles.main}>
-        <p className={styles.test}>cleeti</p>
+      <div className={styles.loginBox}>
+          <h2>login</h2>
+          <form>
+            <div className={styles.inputGroup}>
+              <label htmlFor="email">email</label>
+              <input type="text" id="email" placeholder="Email" />
+            </div>
+            <div className={styles.inputGroup}>
+              <label htmlFor="password">password</label>
+              <input type="password" id="password" placeholder="Password" />
+            </div>
+            <button type="submit" className={styles.loginButton}>Login</button>
+          </form>
+          <p>don't have a login, but need one? contact league staff on discord!</p>
+        </div>
 
       </main>
       <footer className={styles.footer}>
