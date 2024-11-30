a90e289e90b114c32f3add9967004d78d7682020
Pat-TX <williams.patrick.d@gmail.com>
2024-11-30 01:58:46 -0600

    test

--- app/welcome/page.js
+++ app/welcome/page.js
@@ -4,7 +4,7 @@ export default function Welcome() {
   return (
     <div>
       <h1>Welcome to Our Platform!</h1>
-      <p>We’re glad to have you here. Let’s get started!</p>
+      <p>We’re glad to have you here. Let’s get started! :D</p>
     </div>
   );
 }
