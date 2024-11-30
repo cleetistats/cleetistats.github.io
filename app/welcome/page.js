712bc6a73eb8d265070f76ffbe0d2126804629e2
Pat-TX <williams.patrick.d@gmail.com>
2024-11-30 03:30:23 -0600

    fleshed out welcome page
    still need database logic

--- app/welcome/page.js
+++ app/welcome/page.js
@@ -1,14 +1,33 @@
 "use client";
 
 import ProtectedRoute from "../ProtectedRoute";
+import styles from "../page.module.css";
 
 export default function Welcome() {
   return (
     <ProtectedRoute>
-    <div>
-      <h1>Welcome to Our Platform!</h1>
-      <p>We’re glad to have you here. Let’s get started! :D</p>
-    </div>
+      <div className={styles.page}>
+        <main className={styles.main}>
+          <div className={styles.loginBox}>
+            <div className={styles.imageWrapper}>
+              <img src="/PFP.png" alt="Welcome graphic" className={styles.image} />
+            </div>
+            <h2>Welcome in!</h2>
+            <p className={styles.desc}>We just gotta do some administrative tasks, then we'll take you to the dashboard.</p>
+            <form>
+              <div className={styles.inputGroup}>
+                <label htmlFor="username">username</label>
+                <input type="text" id="username" placeholder="Enter discord or epic ID" required/>
+              </div>
+              <div className={styles.inputGroup}>
+                <label htmlFor="password">new password</label>
+                <input type="text" id="password" placeholder="Anything, just not the one we gave you. :D" required/>
+              </div>
+              <button type="submit" className={styles.loginButton}>Proceed</button>
+            </form>
+          </div>
+        </main>
+      </div>
     </ProtectedRoute>
   );
 }
