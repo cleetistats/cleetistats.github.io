3058f4b43d319965ad7d1f4c1db1655e2448a5be
Pat-TX <williams.patrick.d@gmail.com>
2024-11-29 23:04:51 -0600

    added pages

---
+++ app/dashboard/page.js
@@ -0,0 +1,10 @@
+"use client";
+
+export default function Dashboard() {
+  return (
+    <div>
+      <h1>Your Dashboard</h1>
+      <p>Welcome back!</p>
+    </div>
+  );
+}
---
+++ app/welcome/page.js
@@ -0,0 +1,10 @@
+"use client";
+
+export default function Welcome() {
+  return (
+    <div>
+      <h1>Welcome to Our Platform!</h1>
+      <p>We’re glad to have you here. Let’s get started!</p>
+    </div>
+  );
+}
